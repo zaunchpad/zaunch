1174e4dc6e6701a49bcbe61c0f8fa6b339622209
nullmatrix0 <nullmatrixz@proton.me>
2025-12-04 13:29:51 +0700

    chore: remove backend directory from docs branch

--- backend/db/connection.ts
+++
@@ -1,9 +0,0 @@
-import { drizzle } from 'drizzle-orm/postgres-js';
-import postgres from 'postgres';
-import * as schema from './schema';
-
-const connectionString = process.env.DATABASE_URL!;
-
-// Disable prefetch as it is not compatible with "Transaction" pool mode
-const client = postgres(connectionString, { prepare: false });
-export const db = drizzle(client, { schema }); 
--- backend/src/configs/env.config.ts
+++
@@ -1,4 +0,0 @@
-const SOL_NETWORK = process.env.SOL_NETWORK;
-const HELIUS_API_KEY = process.env.HELIUS_API_KEY;
-
-export { SOL_NETWORK, HELIUS_API_KEY };
