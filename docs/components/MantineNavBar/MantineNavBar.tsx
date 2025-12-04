2c3d2598ae70e2d335f0283383b2e23784030cf5
nullmatrix0 <nullmatrixz@proton.me>
2025-12-04 13:35:10 +0700

    chore: remove comment

--- docs/components/MantineNavBar/MantineNavBar.tsx
+++ docs/components/MantineNavBar/MantineNavBar.tsx
@@ -6,13 +6,6 @@ import { ColorSchemeControl } from '../ColorSchemeControl/ColorSchemeControl';
 import { Logo } from '../Logo/Logo';
 import { MantineNextraThemeObserver } from '../MantineNextraThemeObserver/MantineNextraThemeObserver';
 
-/**
- * You can customize the Nextra NavBar component.
- * Don't forget to use the MantineProvider and MantineNextraThemeObserver components.
- *
- * @since 1.0.0
- *
- */
 export const MantineNavBar = () => {
   const { colorScheme } = useMantineColorScheme();
   
